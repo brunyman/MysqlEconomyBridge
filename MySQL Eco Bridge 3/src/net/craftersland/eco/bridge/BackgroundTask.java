d7170f87c375a93df7eeb03538091e45ba850a62
brunyman <alin_bruny@yahoo.com>
2016-11-20 16:45:20 +0200

    Update to 3.1.0

---
+++ MySQL Eco Bridge 3/src/net/craftersland/eco/bridge/BackgroundTask.java
@@ -0,0 +1,59 @@
+package net.craftersland.eco.bridge;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+
+public class BackgroundTask {
+	
+	private Eco m;
+	private long lastSave = System.currentTimeMillis();
+	
+	public BackgroundTask(Eco m) {
+		this.m = m;
+		runTask();
+	}
+	
+	private void runTask() {
+		if (m.getConfigHandler().getBoolean("General.saveDataTask.enabled") == true) {
+			Eco.log.info("Data save task is enabled.");
+		} else {
+			Eco.log.info("Data save task is disabled.");
+		}
+		Bukkit.getScheduler().runTaskTimerAsynchronously(m, new Runnable() {
+
+			@Override
+			public void run() {
+				m.getEcoDataHandler().updateBalanceMap();
+				runSaveData();
+			}
+			
+		}, 20L, 20L);
+	}
+	
+	private void runSaveData() {
+		if (m.getConfigHandler().getBoolean("General.saveDataTask.enabled") == true) {
+			if (Bukkit.getOnlinePlayers().isEmpty() == false) {
+				if (System.currentTimeMillis() - lastSave >= m.getConfigHandler().getInteger("General.saveDataTask.interval") * 60 * 1000) {
+					List<Player> onlinePlayers = new ArrayList<Player>(Bukkit.getOnlinePlayers());
+					if (m.getConfigHandler().getBoolean("General.saveDataTask.hideLogMessages") == false) {
+						Eco.log.info("Saving online players data...");
+					}
+					for (Player p : onlinePlayers) {
+						if (p.isOnline() == true) {
+							m.getEcoDataHandler().onDataSaveFunction(p, false, "false", false);
+						}
+					}
+					if (m.getConfigHandler().getBoolean("General.saveDataTask.hideLogMessages") == false) {
+						Eco.log.info("Data save complete for " + onlinePlayers.size() + " players.");
+					}
+					lastSave = System.currentTimeMillis();
+					onlinePlayers.clear();
+				}
+			}
+		}
+	}
+
+}
